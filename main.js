d2c430347e5aae5879755ea153a2230bc3691737
NetanelYaso <netanelyaso12@gmail.com>
2022-05-17 18:59:12 +0300

    array2

---
+++ main.js
@@ -0,0 +1,16 @@
+// function twoArrays(agearray) {
+//     var newarray=[]
+//     for(i=0; i<agearray.length; i++){
+//         if(agearray[i]>20){
+//             newarray.push(agearray[i])
+//         }
+//     }return newarray
+// }
+// console.log(twoArrays([12,5,15,53,54]))
+
+// function myArray(array) {
+    
+//     for(i=0; i<array.length; i++){
+//         array[i] = 
+//     } return array
+// }
